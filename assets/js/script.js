89e6287f4fdcad19e61abb72fd2955dd7d051a2e
bob.w.perez <bob.w.perez@gmail.com>
2021-06-28 23:45:35 -0400

    basic Layout 75% done

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// M.AutoInit();
+
+$(document).ready(function(){
+    $('.sidenav').sidenav({
+        menuWidth: 300,
+        closeOnClick: true,
+        edge: 'right',
+    });
+  });
+       
+
