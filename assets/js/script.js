8757cff1e80522ae8f799d637d19ca5f662c7503
bob.w.perez <bob.w.perez@gmail.com>
2021-06-29 11:42:22 -0400

    added autocomplete search bar with js function to poulate autocomplete values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,36 @@
 
+const paprikaCoinList = [];
+const coinGeckoList = [];
+
+// data for search autocomplete
+var data = { };
+
+
+
+function init() {
+    getCoinList();
+
+}
+
+init();
+
+function getCoinList() {
+    fetch('https://api.coinpaprika.com/v1/coins').then(function(response) {
+        return response.json();
+    }).then(function(info) {
+        console.log(info);
+        info.forEach(coin => {
+            data[coin.name] = null;
+        });
+
+    })
+}
 
 
 
 
 
-
-
-
-
-
-
+// ------- jQuery initializations for Materialize components ---------- //
 
 // M.AutoInit();
 
@@ -20,5 +41,18 @@ $(document).ready(function(){
         edge: 'right',
     });
   });
+
+
+//   var data = {
+//     "Apple": 'APL',
+//     "Microsoft": null,
+//     "Google": 'https://placehold.it/250x250'
+//   }
+
+$(document).ready(function(){
+$('input.autocomplete').autocomplete({
+   data,
+});
+});
        
 
