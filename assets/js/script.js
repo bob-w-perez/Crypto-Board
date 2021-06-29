43fe27ac842e380c2e639e36d60bae369aa28df3
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-06-29 13:04:21 -0400

    Added return to response then method to return a json response and derive data from it; added a new then method for data to console log it

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,4 +67,8 @@ fetch("https://twitter32.p.rapidapi.com/", {
 })
     .then(response => {
         console.log(response);
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
     })
