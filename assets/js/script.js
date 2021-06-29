90d81909ec10a469e90fa3756a2cb86d050f719f
bob.w.perez <bob.w.perez@gmail.com>
2021-06-29 13:06:11 -0400

    term collapse working decently. needs some style fixing

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,22 +9,49 @@ var data = { };
 
 function init() {
     getCoinList();
-
+    makeTermGlossary();
 }
 
 init();
 
 // look into adding symbol link instead of 'null'
 function getCoinList() {
-    fetch('https://api.coinpaprika.com/v1/coins').then(function(response) {
+    fetch('https://api.coinpaprika.com/v1/tickers').then(function(response) {
+        return response.json();
+    }).then(function(info) {
+        // console.log(info);
+        info.forEach(coin => {
+            data[coin.name] = null;
+            data[coin.symbol] = null;
+        });
+
+    })
+}
+
+// would be good to have the alphabetized
+function makeTermGlossary() {
+    fetch('https://api.coinpaprika.com/v1/tags').then(function(response) {
         return response.json();
     }).then(function(info) {
         console.log(info);
-        info.forEach(coin => {
-            data[coin.name] = null;
-            data[coin.symbol] = null
-        });
+        info.forEach(term => {
+            let termItem = document.createElement('li');
+            let itemHeadCollapse = document.createElement('div');
+            let itemBodyCollapse = document.createElement('div');
+            // let itemBodyCollapseSpan = document.createElement('span');
 
+            itemHeadCollapse.textContent = term.name;
+            termItem.classList.add('collapsible-header');
+            termItem.append(itemHeadCollapse);
+
+            itemBodyCollapse.classList.add('collapsible-body');
+
+            itemBodyCollapse.textContent = term.description;
+            // itemBodyCollapse.append(itemBodyCollapseSpan);
+            termItem.append(itemBodyCollapse);
+
+            $('#term-glossary').append(termItem);
+        });
     })
 }
 
@@ -40,21 +67,17 @@ $(document).ready(function(){
     $('.sidenav').sidenav({
         menuWidth: 300,
         closeOnClick: true,
-        edge: 'right',
+        // edge: 'right',
     });
   });
 
-
-//   var data = {
-//     "Apple": 'APL',
-//     "Microsoft": null,
-//     "Google": 'https://placehold.it/250x250'
-//   }
 
 $(document).ready(function(){
 $('input.autocomplete').autocomplete({
    data,
 });
 });
-       
-
+      
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
