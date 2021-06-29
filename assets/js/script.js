654003207700dff54040fa63cc2b9cbe43deb0f5
David Ludwik <davidludwik237@gmail.com>
2021-06-29 16:50:35 -0400

    card functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var queryEl = document.getElementById('autocomplete-input');
-
+var coins = null;
+var activeCoins = [];
 
 const paprikaCoinList = [];
 const coinGeckoList = [];
@@ -16,12 +17,15 @@ function init() {
 
 init();
 
+
 // look into adding symbol link instead of 'null'
 function getCoinList() {
     fetch('https://api.coinpaprika.com/v1/tickers').then(function(response) {
         return response.json();
     }).then(function(info) {
         // console.log(info);
+        coins = info;
+        console.log(coins);
         info.forEach(coin => {
             data[coin.name] = null;
             data[coin.symbol] = null;
@@ -57,32 +61,56 @@ function makeTermGlossary() {
         });
     })
 }
-=======
+
+function genCoinCard(coin){
+    var coinsArea = document.getElementById('card-space');
+
+    var name = coin.name;
+    
+
+    var coinString = "<p>Name: "+coin.name+"</p>"+
+                    "<p>Symbol: "+coin.symbol+"</p>"+
+                    "<p>Price: "+coin.price+"</p>"+
+                    "<p>Market Cap: "+coin.mktcap+"</p>"+
+                    "<p>All time high: "+coin.ath+"</p>"+
+                    "<p>24H Volume: "+coin.volume+"</p>"+
+                    "<p>Rank: "+coin.rank+"</p>"+
+                    "<p>Supply: "+coin.supply;
+
+    var newCoin = document.createElement('div');
+    newCoin.innerHTML = "<div class=\"row\"><div class=\"col s12 m6\"><div class=\"card\"><div class=\"card-image\"><img src=\"../assets/images/favicon-1.png\"><span class=\"card-title\">Card Title</span><a class=\"btn-floating halfway-fab waves-effect waves-light red\"><i class=\"material-icons\">add</i></a></div><div class=\"card-content\"><p>"+coinString+"</p></div></div></div></div>";
+
+    coinsArea.appendChild(newCoin);
+
+
+
+
+}
+
+
 queryEl.addEventListener("keypress", function(event){
     console.log('activated')
-    if(event.key === 'Enter'){
-        
+
+
+    if(event.key === 'Enter' && coins){
         var query = queryEl.value.toLowerCase();
-        console.log(query);
-        var apiUrl = 'https://api.coinpaprika.com/v1/tickers';
+        
+        var coin = {
+            name: 'placeholder',
+            symbol: 'placeholder',
+            price: 'placeholder',
+            mktcap: 'placeholder',
+            ath: 'placeholder',
+            volume: 'placeholder',
+            rank: 'placeholder',
+            supply: 'placeholder'
+        }
 
-        fetch(apiUrl).then(function(response) {
-            return response.json();
-        }).then(function(data) {
-            const coins = data;
-            var coin = {
-                name: 'placeholder',
-                symbol: 'placeholder',
-                price: 'placeholder',
-                mktcap: 'placeholder',
-                ath: 'placeholder',
-                volume: 'placeholder',
-                rank: 'placeholder',
-                supply: 'placeholder'
-            }
+        for(i=0; i<coins.length; i++){
+            if(coins[i].name.toLowerCase() === query || coins[i].symbol.toLowerCase() === query){
+                if(!activeCoins.includes(coins[i].name)){
+                    
 
-            for(i=0; i<coins.length; i++){
-                if(coins[i].name.toLowerCase() === query || coins[i].symbol.toLowerCase() === query){
                     coin.name = coins[i].name;
                     coin.symbol = coins[i].symbol;
                     coin.price = coins[i].quotes.USD.price;
@@ -92,13 +120,21 @@ queryEl.addEventListener("keypress", function(event){
                     coin.rank = coins[i].rank;
                     coin.supply = coins[i].circulating_supply;
                     
-                    console.log('activated base')
+                    
+                    activeCoins.push(coin.name);
+                    console.log(activeCoins);
+                    genCoinCard(coin);
                     break;
                 }
+                
             }
-            // return coin;
-            console.log(coin); 
-        })
+        }
+
+        
+
+        // return coin;
+        console.log(coin); 
+        
     }
     
 
@@ -158,7 +194,37 @@ $('input.autocomplete').autocomplete({
    data,
 });
 });
+
+       
+// ------- Twitter Feed Fetch -------- //
+var hashtag = 'doge';
+var startDate = '2021-06-29';
+var endDate = '2021-06-30';
+
+
+var endPoint = "/getSearch?" + "hashtag=" + hashtag + "&start_date=" + startDate + "&end_date=" + endDate;
+
+function twitterfetch() {
+    fetch("https://twitter32.p.rapidapi.com" + endPoint, {
+    method: "GET",
+    "headers": {
+        "x-rapidapi-key": "9ce9da8239mshfdc240a5706e6dbp1a372ajsnf408cd27ddc9",
+		"x-rapidapi-host": "twitter32.p.rapidapi.com"
+    }
+})
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.data.tweets);
+    })
+}
+
+//Add Event listener for twitterFetch function
+
       
 $(document).ready(function(){
     $('.collapsible').collapsible();
   });
+
